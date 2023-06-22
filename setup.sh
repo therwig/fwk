3ab338d8817ab859ab1be507c76ead815ff3afb4
Christian Herwig <therwig@users.noreply.github.com>
2023-06-22 17:03:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # add to python path
 echo old $PYTHONPATH
 export PYTHONPATH="${PYTHONPATH}:`pwd`/Common/"
-export PYTHONPATH="${PYTHONPATH}:/Users/therwig/work/commonRootTools/"
+export PYTHONPATH="${PYTHONPATH}:`pwd`/commonRootTools/"
 echo new $PYTHONPATH
